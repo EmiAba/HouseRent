23ddc0bad33fc217460ad67d728069883a56cdda
EmiAba <abadjieva@gmail.com>
2026-02-08 13:58:25 +0200

    repo init

--- HouseRentingSystem.Services/obj/Debug/net8.0/HouseRentingSystem.Services.AssemblyInfo.cs
+++ HouseRentingSystem.Services/obj/Debug/net8.0/HouseRentingSystem.Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HouseRentingSystem.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fecfff0a114b86624dabb7bc1c5ce2fcac43acb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("HouseRentingSystem.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HouseRentingSystem.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
